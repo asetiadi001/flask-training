bec2e328c84db5d79661049700d6f1f2d9626d97
aswin_setiadi <asetiadi001@e.ntu.edu.sg>
2018-10-08 15:12:26 +0800

    Flask tutorial chapter 2 -templates

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,21 @@
+from flask import render_template
 from app import app
 
 
-#first view function
+# first view function
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    users = {'username': 'Aswin Setiadi'}
+    posts = [
+        {
+            'author': {'username': 'Patricia'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Hina'},
+            'body': 'Feed me.'
+        }
 
+    ]
+    return render_template('index.html', title='Home', users=users, posts=posts)
