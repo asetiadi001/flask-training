5983eeebe3c38c03124475d832be7d8caa008a91
aswin_setiadi <asetiadi001@e.ntu.edu.sg>
2018-10-08 17:45:39 +0800

    Flask tutorial chapter 3 -web forms and url_for

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 #workaround for circular import, so it is declared after initializing app variable
 from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 from flask import render_template
 from app import app
-
-
+from app.forms import LoginForm
+from flask import flash, redirect
+from flask import url_for
 # first view function
+
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -19,3 +22,22 @@ def index():
 
     ]
     return render_template('index.html', title='Home', users=users, posts=posts)
+
+
+"""
+-Methods is provided to override accepted default method which is only GET
+
+-One problem with writing links directly in templates and source files is that if one day you decide to reorganize your 
+links, then you are going to have to search and replace these links in your entire application.
+"""
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {} with password {} , remember_me={}'.format(
+            form.username.data, form.password.data, form.remember_me.data
+        ))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
